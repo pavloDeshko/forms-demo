9afd1a559ecb618740d7bc38429e80016660dbb1
Pavlo Deshko <pavlodeshko92@gmail.com>
2020-02-28 20:20:17 +0200

    Updates

---
+++ precache-manifest.d2f968ca52d5099ca3d5d174738095bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b16d0558d766fe5d74889eea9a3c0952",
+    "url": "/forms-demo/index.html"
+  },
+  {
+    "revision": "0184f4414054d63f8192",
+    "url": "/forms-demo/static/css/2.d79ad459.chunk.css"
+  },
+  {
+    "revision": "f62d091c2991c30bb145",
+    "url": "/forms-demo/static/css/main.f612cba8.chunk.css"
+  },
+  {
+    "revision": "0184f4414054d63f8192",
+    "url": "/forms-demo/static/js/2.54739f4a.chunk.js"
+  },
+  {
+    "revision": "03b961c6b8fdd8b1a4bd94a65d4327ff",
+    "url": "/forms-demo/static/js/2.54739f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62d091c2991c30bb145",
+    "url": "/forms-demo/static/js/main.66dd12c0.chunk.js"
+  },
+  {
+    "revision": "b3f3f759d7047d675333",
+    "url": "/forms-demo/static/js/runtime-main.9440da7d.js"
+  }
+]);
